0b4e0f6ab492ec6debdc97dad66121e8b9ac3d7a
Francesca Rodriguez Sawaya <francesca.rodriguezsawaya@gmail.com>
2020-09-15 12:00:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let drawing = [];
 let currentPath = [];
 
 function setup(){
-  canvas = createCanvas (windowWidth,(windowHeight - 100px));
+  canvas = createCanvas (windowWidth,(windowHeight - 100));
   canvas.mousePressed(startPath);
   canvas.touchStarted(startPath);
   canvas.parent('canvascontainer');
